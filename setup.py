78b514d1bfa2483360eef2699e600a3ab6f0871d
unknown <f2954@npc-pc1-f2954.nikonoa.net>
2014-10-14 15:43:22 +0800

    modify version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 setup(
     name = 'zhuaxia',
-    version = '1.0.0',
+    version = '2.0.0',
     install_requires=[ 'requests','mutagen' ],
     packages = find_packages(),
     package_data={'zhuaxia':['conf/*.conf']},
